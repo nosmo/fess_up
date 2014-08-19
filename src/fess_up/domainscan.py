642178a5c81e9273f54762328df01c4b3f06df4e
Hugh <nosmo@nosmo.me>
2014-08-19 15:40:02 +0000

    More durable resolver use. Correct handling of lists returned by dnspython to avoid dual-listing

--- src/fess_up/domainscan.py
+++ src/fess_up/domainscan.py
@@ -20,6 +20,8 @@ class DomainScan(object):
         self.domain = domain
         self.subdomain_list = subdomain_list
         self.data = collections.defaultdict(dict)
+        self.resolver = dns.resolver.Resolver()
+        self.resolver.retry_servfail = True
 
     def runScan(self):
 
@@ -32,7 +34,7 @@ class DomainScan(object):
 
         for subdomain, records in self._scan("CNAME", subdomains=self.data.keys()).iteritems():
             self.data[subdomain]["CNAME"] = [ str(record) for record in records ]
-
+            
         mxlist = []
         for subdomain, records in self._scan("MX", subdomains=self.data.keys()).iteritems():
             for i in xrange(0, len(records), 2):
@@ -42,7 +44,7 @@ class DomainScan(object):
             self.data[subdomain]["MX"] = mxlist
 
         for subdomain, records in self._scan("TXT", subdomains=self.data.keys()).iteritems():
-            self.data[subdomain]["TXT"] = [ str(record) for record in records ]
+            self.data[subdomain]["TXT"] = records
 
     def _scan(self, record_type, subdomains=None):
         results = collections.defaultdict(list)
@@ -52,7 +54,7 @@ class DomainScan(object):
         for subdomain in subdomains:
             query_str = "%s.%s" % (subdomain, self.domain) if subdomain else self.domain
             try:
-                answers = dns.resolver.query("%s" % (query_str), record_type)
+                answers = self.resolver.query("%s" % (query_str), record_type)
             except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer,
                     dns.name.EmptyLabel) as e:
                 continue
@@ -61,14 +63,18 @@ class DomainScan(object):
             for data in answers:
                 record_valuenames = dnsobject_map[type(data)]
                 for record_valuename in record_valuenames:
-                    record_results.append(getattr(data, record_valuename))
+                    record_data = getattr(data, record_valuename)
+                    if type(record_data) == list: 
+                        record_results += record_data
+                    else:
+                        record_results.append(record_data)
             results[subdomain] = record_results
 
         return dict(results)
 
     def _checkWildcards(self):
         try:
-            answers = dns.resolver.query(
+            answers = self.resolver.query(
                 "trollllloolololoololo1337lolololololollol.%s" % self.domain
             )
         except dns.resolver.NXDOMAIN as e:
