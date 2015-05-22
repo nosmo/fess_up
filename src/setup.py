07381404feb364b959b83ccc869f5ad94ec41a7f
Hugh Nowlan <nosmo@nosmo.me>
2015-05-22 11:57:36 +0100

    Get rid of pip-breaking read() function, add long_description manually

--- src/setup.py
+++ src/setup.py
@@ -10,15 +10,20 @@ def read(fname):
 
 setup(
     name = "fess_up",
-    version = "1.0.0",
+    version = "1.1.1",
     author = "Hugh Nowlan",
     author_email = "nosmo@nosmo.me",
     description = "DNS record scraper",
+    long_description = """Fess Up is an unintelligent DNS record
+guesser. It offers both a library and a command line tool to scan
+specific domains for (currently) one layer of subdomains. The scan is
+dictionary based and it will attempt to avoid superfluous queries by
+basing subsequent queries off of previously discovered records.
+""",
     license = "Hacktivismo Enhanced-Source Software License Agreement",
     keywords = "dns zone scraper",
     url = "http://github.com/nosmo/fess_up",
     packages=['fess_up', 'fess_up.dnsnames'],
-    long_description=read('../README.md'),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
